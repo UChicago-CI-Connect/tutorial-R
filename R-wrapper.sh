ead02208d22d12a6251897b1721bad2c85f7e13c
Anton Yu <antonyu@uchicago.edu>
2014-09-08 14:58:39 -0500

    fixed typo

--- R-wrapper.sh
+++ R-wrapper.sh
@@ -6,7 +6,7 @@ if [ $# -ne $EXPECTED_ARGS ]; then
   echo "Usage: R-wrapper.sh file.R"
   exit 1
 else
-  source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/bash
+  source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
   module load R
   Rscript $1
 fi
